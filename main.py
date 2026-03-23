5e68a75f03ec8acf94d471bc1dbf76ba303568bb
GeraldLGH <GLuettgens@web.de>
2026-03-23 14:05:13 +0100

    Refactor login script and improve comments

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from playwright.sync_api import sync_playwright, TimeoutError
-
-# Login-Daten
-USERNAME = "g.luettgens@itergo.com"
-PASSWORD = "m00racL.6005"
-
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
 
-    # 1️⃣ Login
+    # Seite aufrufen
     page.goto("https://duesseldorf.ergoladesaeulen.de/wp-login.php")
     page.fill('input[name="log"]', USERNAME)
     page.fill('input[name="pwd"]', PASSWORD)
@@ -18,16 +12,14 @@ with sync_playwright() as p:
     page.wait_for_load_state('networkidle')
 
 
-    ### zum Test
-    # page.screenshot(path="screenshot.png")
-    # print("Screenshot gespeichert: screenshot.png")
-
+    # Screenshot speichern
     page.screenshot(path="screenshot.png")
-with open("page.html", "w", encoding="utf-8") as f:
-    f.write(page.content())
-page.wait_for_timeout(3000)
-print("Screenshot und HTML gespeichert")
 
+    # HTML speichern
+    with open("page.html", "w", encoding="utf-8") as f:
+        f.write(page.content())
 
+    print("Screenshot und HTML gespeichert")
 
+    # Browser schließen
     browser.close()
